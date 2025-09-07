d3afefa7ce7debb4838691c74128a5f04d3a51c7
Pattarapong Chinwatcharaset (Noah) <168044305+PattarapongNoah435@users.noreply.github.com>
2025-09-07 11:45:11 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Script for navigation bar
+
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+
+const nav = document.getElementById("navbar");
+
+if (bar) {
+    bar.addEventListener("click", (e) => {
+        e.preventDefault();
+        nav.classList.add("active");
+    });
+}
+
+if (close) {
+    close.addEventListener("click", (e) => {
+        e.preventDefault();
+        nav.classList.remove("active");
+    });
+}
+
